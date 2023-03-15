5d75116a3dc4bf5a71f2b32137f06d0ed8959a71
Vazha Mamatsashvili <vmama20@freeuni.edu.ge>
2023-03-15 14:53:32 +0400

    Moved the helper methods to a separate file

--- analyzer/code_analyzer.py
+++ analyzer/code_analyzer.py
@@ -1,40 +1,13 @@
-import ast
 import sys
 import os
+import ast
+
 from analyzer import Analyzer
 from regex_matcher import RegexMatcher
 from Error import ErrorLogger
+from helper import get_file_paths, not_in_string
 
 MAX_LINE_LENGTH = 79
-
-
-# loops through the line and finds if the given character is part of a string or not.
-# Returns index or None if not found.
-def not_in_string(line, char):
-    in_string = False
-    last_char = None
-    for i in range(len(line)):
-        if (line[i] == '\'' or line[i] == '\"') and line[i - 1] != '\\':
-            if last_char is None:
-                last_char = line[i]
-                in_string = True
-            elif line[i] == last_char:
-                in_string = False
-                last_char = None
-            continue
-        if line[i] == char and not in_string:
-            return i
-
-    return None
-
-
-def get_file_paths(path):
-    paths = []
-    for (path, _, filenames) in os.walk(path):
-        for filename in filenames:
-            if filename.endswith('.py'):
-                paths.append(os.path.join('.', path, filename))
-    return paths
 
 
 class StaticCodeAnalyzer:
